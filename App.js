e23caecaac578a0b1737b2690670e3543996e1b1
JoaoGomes <joaoeduardocostagomes@hotmail.com>
2021-08-07 14:48:04 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ScrollViewExample from './ScrollViewExample';
+import FlatListExample from './FlatListExample';
+import SectionListExample from './SectionListExample';
+
+export default function App() {
+	return (
+    //<ScrollViewExample/>
+ 		//<FlatListExample />
+    <SectionListExample />
+	);
+}
